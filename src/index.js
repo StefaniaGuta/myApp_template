f3142d8f112f84e8dca0ae09544b3f3024308306
StefaniaGuta <142820560+StefaniaGuta@users.noreply.github.com>
2024-09-23 21:34:56 +0300

    change basename

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/">
+    <BrowserRouter basename="myApp_template">
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
           <App />
